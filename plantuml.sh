97c245e9e2b88af2c5cafcf230ca82d5d248c55d
James Booth <jeboothjr@gmail.com>
2023-08-16 16:16:24 -0500

    Updating PlantUML with security flag

--- plantuml.sh
+++ plantuml.sh
@@ -2,4 +2,7 @@
 
 echo $$;
 
-docker run -p 8080:8080 plantuml/plantuml-server:tomcat-v1.2022.6
+# https://plantuml.com/security
+docker run --env PLANTUML_SECURITY_PROFILE=LEGACY -p 8080:8080 plantuml/plantuml-server:tomcat-v1.2023.10
+
+# docker run -p 8080:8080 plantuml/plantuml-server:tomcat-v1.2022.6
