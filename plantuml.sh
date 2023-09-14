311531ade4f9148095105031bf101b0968f443e0
James Booth <jeboothjr@gmail.com>
2023-09-14 13:34:38 -0500

    Adding name to container

--- plantuml.sh
+++ plantuml.sh
@@ -3,6 +3,6 @@
 echo $$;
 
 # https://plantuml.com/security
-docker run --env PLANTUML_SECURITY_PROFILE=LEGACY -p 8080:8080 plantuml/plantuml-server:tomcat-v1.2023.10
+docker run --env PLANTUML_SECURITY_PROFILE=LEGACY --name plantuml -p 8080:8080 plantuml/plantuml-server:tomcat-v1.2023.10
 
 # docker run -p 8080:8080 plantuml/plantuml-server:tomcat-v1.2022.6
