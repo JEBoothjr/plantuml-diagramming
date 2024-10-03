36f747abe0122b617994375f127e199a71dd9ca1
James Booth <jeboothjr@gmail.com>
2024-10-03 09:29:25 -0500

    Update data and other tweaks

--- plantuml.sh
+++ plantuml.sh
@@ -5,6 +5,7 @@ echo $$;
 docker rm plantuml
 
 # https://plantuml.com/security
-docker run --env PLANTUML_SECURITY_PROFILE=LEGACY --name plantuml -p 6200:8080 plantuml/plantuml-server:tomcat-v1.2023.10
+docker run --env PLANTUML_SECURITY_PROFILE=LEGACY --name plantuml -p 6200:8080 plantuml/plantuml-server:tomcat-v1.2024.4
+# docker run --env PLANTUML_SECURITY_PROFILE=LEGACY --name plantuml -p 6200:8080 plantuml/plantuml-server:tomcat-v1.2023.10
 
 # docker run -p 6200:6200 plantuml/plantuml-server:tomcat-v1.2022.6
